fb4a72064ca7414b6b698356e5732dbee401ab63
solomonrazak <solomonrazak99@gmail.com>
2024-12-05 17:51:53 -0800

    Updated navlink styling

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
-const Navbar:React.FC = () => {
+const Navbar: React.FC = () => {
+  const location = useLocation();
+
+  const routeNames: Record<string, string> = {
+    "/dashboard": "Dashboard",
+    "/customers": "Customers",
+    "/charging-points": "Charging Points",
+  };
+
+  // Get the name based on the current path
+  const currentPage = routeNames[location.pathname] || "Unknown";
+
+  const name = "Solomon Razak";
+  const initial = name.charAt(0);
+
   return (
     <div className="p-2 border-b flex justify-between border-white w-full">
-        <p>Dashboard</p>
-        <div>
-            <p>Solomon Razak</p>
-        </div>
+      <p>{currentPage}</p>
+      <div className="flex items-center gap-2">
+        <div className="w-7 h-7 flex justify-center items-center font-medium bg-green-400 text-white rounded-full border border-white">{initial}</div>
+        <p className="font-medium">{name}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,26 +5,25 @@ import Customers from "./pages/customers";
 import ChargingPoints from "./pages/chargingPoints";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                index: true,
-                path: "/dashboard",
-                element: <Dashboard />,
-
-            },
-            {
-                path: "/customers",
-                element: <Customers />
-            },
-            {
-                path: "/charging-points",
-                element: <ChargingPoints />
-            }
-        ]
-    }
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/customers",
+        element: <Customers />,
+      },
+      {
+        path: "/charging-points",
+        element: <ChargingPoints />,
+      },
+    ],
+  },
 ]);
 
 export default router;
